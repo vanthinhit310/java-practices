4f0c8969df8e9606c030371a78e8bd8faf70d12c
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2022-03-12 23:44:26 +0700

    -
    -

---
+++ MathClass/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 3.14;
+		double y = 10;
+		
+//		double z = Math.max(x, y);
+//		double z = Math.sqrt(y);
+//		double z = Math.round(x);
+		double z = Math.floor(x);
+		
+		System.out.println("Value of Z is: " + z);
+	}
+
+}
