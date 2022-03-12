22f2ca5930b76e70a0204eafc934714495a60d10
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2022-03-12 23:36:51 +0700

    -
    -

--- Expressions/src/Main.java
+++ Expressions/src/Main.java
@@ -3,6 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int friends = 10;
+//		friends++;
+//		friends--;
+//		friends += 5;
+//		friends -= 5;
+//		friends *= 5;
+//		friends /= 5;
+		friends %= 5;
+		
+		System.out.println("Friend is: " + friends);
 
 	}
 
